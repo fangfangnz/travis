d5dfb27d1f53815295b9b1db551dea50e8e3d9ef
fang <qifeng.fang@qq.com>
2020-03-06 11:29:49 +1300

    second commit

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Login
+{
+	public static void main(String[] args){
+		System.out.println("Welcome User to ASB!");
+		System.out.println("@_@About US Fang@_@");
+                                System.out.println("RBNZ");
+
+	}
+}
